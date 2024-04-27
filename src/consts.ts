77d02438d084e7f9a7c1e38c9a3aeb24ae88f006
Jimmy Hu <jimmyhu.coding@gmail.com>
2024-04-27 22:56:01 +0800

    temp

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '不具名工程師日誌';
+export const SITE_DESCRIPTION = '分享那些開發大小事!';
---
+++ src/content/blog/240427-first-post.md
@@ -0,0 +1,6 @@
+---
+title: '旅途的起點'
+description: '不具名工程師日誌第一篇文章'
+pubDate: '2024 04 27'
+heroImage: '/240427.webp'
+---
